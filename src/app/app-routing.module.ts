16429c32eed2694d5d2800879b28b1c34386e522
mukilan87 <mukilnithin@gmail.com>
2022-03-01 13:25:01 +0530

    Phase 4 Main Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooditemsComponent } from './fooditems/fooditems.component';
+import { HomeComponent } from './home/home.component';
+import { PaymentComponent } from './payment/payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'fooditems',component:FooditemsComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'payment',component:PaymentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FooditemsComponent } from './fooditems/fooditems.component';
+import { ContactComponent } from './contact/contact.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    FooditemsComponent,
+    ContactComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
